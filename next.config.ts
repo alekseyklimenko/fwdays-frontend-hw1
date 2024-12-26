00cdf4cebc85fc872b3961b91982ecd5351079ec
Oleksii Klymenko <alex.klimenko@gmail.com>
2024-12-26 10:41:51 +0200

    add supabase and shadcn/ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://skjngkdmmncxcbpcsujs.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNram5na2RtbW5jeGNicGNzdWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxMjYwMjAsImV4cCI6MjA1MDcwMjAyMH0.a_U_4HqfMTN6W1NaytRyngp-BTAI6uuJ9wwYFAOSFbA"
+    }
 };
 
 export default nextConfig;
