c2ddd81eeaaac40296b0a73aad4fe604b7aa58b4
Oleksii Klymenko <alex.klimenko@gmail.com>
2025-01-04 14:02:10 +0200

    add dockerfile

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
     env: {
         SUPABASE_URL: "https://skjngkdmmncxcbpcsujs.supabase.co",
         SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNram5na2RtbW5jeGNicGNzdWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxMjYwMjAsImV4cCI6MjA1MDcwMjAyMH0.a_U_4HqfMTN6W1NaytRyngp-BTAI6uuJ9wwYFAOSFbA"
-    }
+    },
+    output: "standalone"
 };
 
 export default nextConfig;
